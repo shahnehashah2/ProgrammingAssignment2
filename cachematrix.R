3cc4eef2a6b82cb0d68f6cd8b5d3b91457747885
shahnehashah2 <shahnehashah2@gmail.com>
2017-01-05 00:39:56 -0600

    Completed Week3 Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Helpful guide: https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
 
-## Write a short comment describing this function
+## makeCacheMatrix: Sets and retrieves the cached inverse matrix; if present
+## cacheSolve: Display the inverse of a matrix. Use makeCacheMatrix to 
+## set the inverse in cache and retrieve from cache
+
+## Setting the setters and getters in this function. 'inv' stores the inverse 
+## of the matrix 'x'. A list of the function names and the values of x and inv 
+## are returned.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- matrix()
+    
+    # x and inv variables in parent function cacheSolve are set
+    set <- function(y) {    
+        x <<- y
+        inv <<- matrix()
+    }
+    
+    # Inverse calculated in cacheSolve() is set to inv 
+    setinv <- function(inverse) inv <<- inverse
+    
+    get <- function() x
+    getinv <- function() inv
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. If a non-square matrix is 
+## provided, throw an error.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    data <- x$get()
+    
+    # Display an error message and stop execution if matrix is not square
+    if (nrow(data) != ncol(data)) {
+        stop("Not a square matrix")
+    }
+    
+    # Check whether the values of inverse matrix are NA. 'all' checks for NA 
+    # in each (row,col) value
+    if(all(!is.na(inv))) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    # Setting tolerance really low to allow inverse computation of small numbers
+    inv <- solve(data, tol = 1e-20)
+    x$setinv(inv)
+    inv
 }
